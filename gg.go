82cb0fdd423ddf8c8df90307a0e0a69c8b4e6fbe
Johan Walles <johan.walles@gmail.com>
2023-01-13 21:25:48 +0100

    Handle unset variables as failures

--- gg.go
+++ gg.go
@@ -1,18 +1,32 @@
 package gg
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // Get a value from environment and run it through the parse function. Return
-// the result if there was one, or an error if the parsing fails.
+// the result if there was one, or an error if the parsing fails or if the
+// variable was not set.
 func Get[R any](environmentVariableName string, parse func(string) (R, error)) (R, error) {
-	rawValue := os.Getenv(environmentVariableName)
+	rawValue, found := os.LookupEnv(environmentVariableName)
+	if !found {
+		var noResult R
+		return noResult, fmt.Errorf("Environment variable not set: %s", environmentVariableName)
+	}
+
 	return parse(rawValue)
 }
 
 // Get a value from environment and run it through the parse function. Return
-// the result if there was one, fallback if the parsing fails.
+// the result if there was one, fallback if the parsing fails or if the variable
+// was not set.
 func GetOr[R any](environmentVariableName string, parse func(string) (R, error), fallback R) R {
-	rawValue := os.Getenv(environmentVariableName)
+	rawValue, found := os.LookupEnv(environmentVariableName)
+	if !found {
+		return fallback
+	}
+
 	parsedValue, err := parse(rawValue)
 	if err != nil {
 		return fallback
