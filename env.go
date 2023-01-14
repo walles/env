7dac1bcf0af0a4df9637d6d8de71c32212cceb94
Johan Walles <johan.walles@gmail.com>
2023-01-14 08:27:33 +0100

    Add examples for each function

--- env.go
+++ env.go
@@ -8,6 +8,10 @@ import (
 // Get a value from environment and run it through the parse function. Return
 // the result if there was one, or an error if the parsing fails or if the
 // variable was not set.
+//
+// # Example Usage
+//
+//	port, err := env.Get("PORT", strconv.Atoi)
 func Get[V any](environmentVariableName string, parse func(string) (V, error)) (V, error) {
 	rawValue, found := os.LookupEnv(environmentVariableName)
 	if !found {
@@ -21,6 +25,10 @@ func Get[V any](environmentVariableName string, parse func(string) (V, error)) (
 // Get a value from environment and run it through the parse function. Return
 // the result if there was one, fallback if the parsing fails or if the variable
 // was not set.
+//
+// # Example Usage
+//
+//	port := env.GetOr("PORT", strconv.Atoi, 8080)
 func GetOr[V any](environmentVariableName string, parse func(string) (V, error), fallback V) V {
 	rawValue, found := os.LookupEnv(environmentVariableName)
 	if !found {
@@ -38,6 +46,10 @@ func GetOr[V any](environmentVariableName string, parse func(string) (V, error),
 // Get a value from environment and run it through the parse function. Return
 // the result if there was one, or panic if the parsing fails or if the variable
 // was not set.
+//
+// # Example Usage
+//
+//	port := env.MustGet("PORT", strconv.Atoi)
 func MustGet[V any](environmentVariableName string, parse func(string) (V, error)) V {
 	parsedValue, err := Get(environmentVariableName, parse)
 	if err != nil {
